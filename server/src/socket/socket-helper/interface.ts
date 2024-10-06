eda0b50426f433d267d07a05a97acbb6ed88c3ac
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-06 05:25:12 +0100

    Refactor interface.ts to import IUser from the Mongodb user.model instead of the Postgres user.model

--- server/src/socket/socket-helper/interface.ts
+++ server/src/socket/socket-helper/interface.ts
@@ -1,3 +1,16 @@
+/* eslint-disable no-unused-vars */
+import { BaseContract, BigNumberish, TransactionResponse } from 'ethers';
+
+export interface PodXContract extends BaseContract {
+    createPodcast(podcastId: string, ipfsContentHash: string): Promise<TransactionResponse>;
+    joinPodcast(podcastId: string): Promise<TransactionResponse>;
+    leavePodcast(podcastId: string): Promise<TransactionResponse>;
+    updatePodcastContent(podcastId: string, newIpfsContentHash: string): Promise<TransactionResponse>;
+    requestCoHost(podcastId: string): Promise<TransactionResponse>;
+    approveCoHost(podcastId: string, coHostUserId: string): Promise<TransactionResponse>;
+    sendTip(podcastId: string, recipientId: string, amount: BigNumberish): Promise<TransactionResponse>;
+    // Add any other methods that your contract has
+}
 
 export interface PodMember {
     userId: string;
