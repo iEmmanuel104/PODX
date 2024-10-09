bbf8fdec62e59f42b44865aea27ec392105a52f3
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-10 00:13:56 +0100

    Refactor: Update user.routes.ts to remove unnecessary middleware and authentication

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -11,7 +11,7 @@ const upload = uploadMiddleware(UploadType.Single, 'file');
 router
     .get('/', AuthenticatedController(UserController.getAllUsers))
     .get('/info', AuthenticatedController(UserController.getUser))
-    .post('/validate', basicAuth(), upload, AuthenticatedController(UserController.findOrCreateUser))
+    .post('/validate', UserController.findOrCreateUser)
     .patch('/update', basicAuth(), upload, AuthenticatedController(UserController.updateUser));
     
 export default router;
