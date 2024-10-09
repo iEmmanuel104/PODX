e931a4521d5dc7ea61214de122f3b24d90d2ea17
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-09 04:15:49 +0100

    Refactor: Add mediaReducer to store configuration

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import podReducer from './slices/podSlice';
 import userReducer from './slices/userSlice';
+import mediaReducer from './slices/mediaSlice';
 
 export const store = configureStore({
     reducer: {
         pod: podReducer,
         user: userReducer,
+        media: mediaReducer,
     },
 });
 
