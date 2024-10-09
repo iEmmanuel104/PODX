42debd8a7a20dbd524cd428ced52bd4b366bb488
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-09 22:41:38 +0100

    Refactor: Add socket slice to store configuration

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -1,19 +1,20 @@
-// src/store/index.ts
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './api/api';
 import userReducer from './slices/userSlice';
 import podReducer from './slices/podSlice';
 import mediaReducer from './slices/mediaSlice';
-
+import socketReducer from './slices/socketSlice';
 
 import type { UserState } from './slices/userSlice';
 import type { PodState } from './slices/podSlice';
-import type { MediaState } from './slices/mediaSlice'
+import type { MediaState } from './slices/mediaSlice';
+import type { SocketState } from './slices/socketSlice';
 
 export interface RootState {
     user: UserState;
     pod: PodState;
     media: MediaState;
+    socket: SocketState;
     [apiSlice.reducerPath]: ReturnType<typeof apiSlice.reducer>;
 }
 
@@ -23,6 +24,7 @@ export const store = configureStore({
         user: userReducer,
         pod: podReducer,
         media: mediaReducer,
+        socket: socketReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
