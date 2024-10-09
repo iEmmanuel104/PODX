e2341b294adf54f47f108a6422624cb39b38ddbc
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-09 22:19:48 +0100

    Refactor: Add constants for server URL, signature message, and Privy app ID

---
+++ frontend/constants/index.ts
@@ -0,0 +1,7 @@
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL as string;
+
+console.log({ SERVER_URL });
+
+export const SIGNATURE_MESSAGE = process.env.NEXT_PUBLIC_SIGNATURE_MESSAGE as string;
+
+export const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID as string;
