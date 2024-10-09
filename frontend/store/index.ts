bc7166185ba56eb86b2a1f6d7fb38c2c4a752c2f
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-09 01:36:18 +0100

    Refactor: Add userReducer to store configuration

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import podReducer from './slices/podSlice';
+import userReducer from './slices/userSlice';
 
 export const store = configureStore({
     reducer: {
         pod: podReducer,
+        user: userReducer,
     },
 });
 
