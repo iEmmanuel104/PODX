195979d21129e492a561de3c21e6f2f9b1cf5a6c
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-10 00:11:40 +0100

    Refactor: Add constant for server socket URL in constants.ts

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,6 +1,8 @@
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL as string;
 
-console.log({ SERVER_URL });
+export const SERVER_SOCKET_URL = process.env.NEXT_PUBLIC_SERVER_SOCKET_URL as string;
+
+console.log({ SERVER_URL, SERVER_SOCKET_URL });
 
 export const SIGNATURE_MESSAGE = process.env.NEXT_PUBLIC_SIGNATURE_MESSAGE as string;
 
