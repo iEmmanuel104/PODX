cd656106d65f69acf675e04aff4845d2780e7908
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-09 04:16:03 +0100

    Add mediaReducer to store configuration

---
+++ frontend/store/slices/mediaSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface MediaState {
+    isAudioEnabled: boolean
+    isVideoEnabled: boolean
+}
+
+const initialState: MediaState = {
+    isAudioEnabled: true,
+    isVideoEnabled: true,
+}
+
+const mediaSlice = createSlice({
+    name: 'media',
+    initialState,
+    reducers: {
+        setAudioEnabled: (state, action: PayloadAction<boolean>) => {
+            state.isAudioEnabled = action.payload
+        },
+        setVideoEnabled: (state, action: PayloadAction<boolean>) => {
+            state.isVideoEnabled = action.payload
+        },
+    },
+})
+
+export const { setAudioEnabled, setVideoEnabled } = mediaSlice.actions
+
+export default mediaSlice.reducer
