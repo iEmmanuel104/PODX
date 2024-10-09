57b9f54019f301dd033035bc72f523c703ae1655
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-09 21:35:36 +0100

    Refactor: Update store configuration to include apiSlice and middleware

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -1,15 +1,32 @@
+// src/store/index.ts
 import { configureStore } from '@reduxjs/toolkit';
-import podReducer from './slices/podSlice';
+import { apiSlice } from './api/api';
 import userReducer from './slices/userSlice';
+import podReducer from './slices/podSlice';
 import mediaReducer from './slices/mediaSlice';
+
+
+import type { UserState } from './slices/userSlice';
+import type { PodState } from './slices/podSlice';
+import type { MediaState } from './slices/mediaSlice'
+
+export interface RootState {
+    user: UserState;
+    pod: PodState;
+    media: MediaState;
+    [apiSlice.reducerPath]: ReturnType<typeof apiSlice.reducer>;
+}
 
 export const store = configureStore({
     reducer: {
-        pod: podReducer,
+        [apiSlice.reducerPath]: apiSlice.reducer,
         user: userReducer,
+        pod: podReducer,
         media: mediaReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: process.env.NODE_ENV !== 'production',
 });
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
