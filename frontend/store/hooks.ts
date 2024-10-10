146bce7058bf4a425739e7778507e3dc49df91dc
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-10 05:42:17 +0100

    Refactor: Add useSocketStatus hook to retrieve socket connection status

--- frontend/store/hooks.ts
+++ frontend/store/hooks.ts
@@ -3,3 +3,7 @@ import type { TypedUseSelectorHook } from 'react-redux'
 import type { RootState, AppDispatch } from './index'
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export const useSocketStatus = () => {
+    return useAppSelector((state) => state.socket.isConnected);
+};
