9ae3e4b9211f26caa248907774b998da8db5dd94
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-11 21:21:55 +0100

    Add SpeechIndicator component for displaying speech indicators in meetings

---
+++ frontend/components/meeting/speechIndicator.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import clsx from "clsx";
+
+interface SpeechIndicatorProps {
+    isSpeaking: boolean;
+    isDominantSpeaker?: boolean;
+}
+
+const SpeechIndicator: React.FC<SpeechIndicatorProps> = ({ isSpeaking, isDominantSpeaker = true }) => {
+    return (
+        <span
+            className={clsx(
+                "str-video__speech-indicator",
+                isDominantSpeaker && "str-video__speech-indicator--dominant",
+                isSpeaking && "str-video__speech-indicator--speaking"
+            )}
+        >
+            <span className="str-video__speech-indicator__bar" />
+            <span className="str-video__speech-indicator__bar" />
+            <span className="str-video__speech-indicator__bar" />
+        </span>
+    );
+};
+
+export default SpeechIndicator;
