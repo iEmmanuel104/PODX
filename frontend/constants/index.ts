4140f439f335f53163febcd7817323af263238df
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-11 21:22:03 +0100

    Refactor: Add STREAM_API_KEY and STREAM_API_SECRET constants

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -7,3 +7,7 @@ console.log({ SERVER_URL, SERVER_SOCKET_URL });
 export const SIGNATURE_MESSAGE = process.env.NEXT_PUBLIC_SIGNATURE_MESSAGE as string;
 
 export const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID as string;
+
+export const STREAM_API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY as string;
+
+export const STREAM_API_SECRET = process.env.NEXT_PUBLIC_STREAM_API_SECRET as string;
