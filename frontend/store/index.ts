f71aa6b3c6c80bf62796c46e91048bbfe270194d
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-12 18:47:40 +0100

    Refactor store/index.ts to include toastReducer in RootState

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -4,17 +4,20 @@ import userReducer from './slices/userSlice';
 import podReducer from './slices/podSlice';
 import mediaReducer from './slices/mediaSlice';
 import socketReducer from './slices/socketSlice';
+import toastReducer from './slices/toastSlice';
 
 import type { UserState } from './slices/userSlice';
 import type { PodState } from './slices/podSlice';
 import type { MediaState } from './slices/mediaSlice';
 import type { SocketState } from './slices/socketSlice';
+import type { toastState } from './slices/toastSlice';
 
 export interface RootState {
     user: UserState;
     pod: PodState;
     media: MediaState;
     socket: SocketState;
+    toast: toastState;
     [apiSlice.reducerPath]: ReturnType<typeof apiSlice.reducer>;
 }
 
@@ -25,6 +28,7 @@ export const store = configureStore({
         pod: podReducer,
         media: mediaReducer,
         socket: socketReducer,
+        toast: toastReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
