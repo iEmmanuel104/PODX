160d3504b93cea949fa531c7db1dfe543f86a0b1
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-12 01:22:40 +0100

    Refactor MeetingPreview component to add support for displaying audio and video device selectors and improve styling

--- frontend/components/meeting/meetingPreview.tsx
+++ frontend/components/meeting/meetingPreview.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { VideoPreview, useCallStateHooks, useConnectedUser, createSoundDetector } from "@stream-io/video-react-sdk";
 import { useAppSelector, useAppDispatch } from "@/store/hooks";
 import { setAudioEnabled, setVideoEnabled, setSoundDetected } from "@/store/slices/mediaSlice";
 import SpeechIndicator from "./speechIndicator";
-import { Mic, MicOff, Video, VideoOff } from "lucide-react";
+import { Mic, MicOff, Video, VideoOff, MoreVertical, Sparkles } from "lucide-react";
+import { AudioInputDeviceSelector, AudioOutputDeviceSelector, VideoInputDeviceSelector } from "./deviceSelector";
+import IconButton from "./iconButton";
 
 const MeetingPreview: React.FC = () => {
     const user = useConnectedUser();
     const dispatch = useAppDispatch();
     const { isAudioEnabled, isVideoEnabled, isSoundDetected } = useAppSelector((state) => state.media);
+    const [displaySelectors, setDisplaySelectors] = useState(false);
+    const [videoPreviewText, setVideoPreviewText] = useState("");
 
     const { useCameraState, useMicrophoneState } = useCallStateHooks();
     const { camera, isMute: isCameraMute, hasBrowserPermission: hasCameraPermission } = useCameraState();
@@ -54,6 +58,13 @@ const MeetingPreview: React.FC = () => {
         };
     }, [microphoneStatus, mediaStream, dispatch]);
 
+    useEffect(() => {
+        if (hasMicrophonePermission === undefined) return;
+        if ((hasMicrophonePermission && microphoneStatus) || !hasMicrophonePermission) {
+            setDisplaySelectors(true);
+        }
+    }, [microphoneStatus, hasMicrophonePermission]);
+
     const toggleAudio = useCallback(() => {
         microphone.toggle().then(() => {
             dispatch(setAudioEnabled(!isAudioEnabled));
@@ -61,21 +72,43 @@ const MeetingPreview: React.FC = () => {
     }, [microphone, dispatch, isAudioEnabled]);
 
     const toggleVideo = useCallback(() => {
+        setVideoPreviewText(isVideoEnabled ? "Camera is off" : "Camera is starting");
         camera.toggle().then(() => {
             dispatch(setVideoEnabled(!isVideoEnabled));
+            setVideoPreviewText(isVideoEnabled ? "Camera is off" : "");
         });
     }, [camera, dispatch, isVideoEnabled]);
 
     return (
-        <div className="w-full bg-[#1D1D1D] p-3.5 rounded-xl">
+        <div className="w-full max-w-3xl lg:pr-2 lg:mt-8">
             <div className="relative w-full rounded-lg aspect-video mx-auto shadow-md">
                 <div className="absolute z-0 left-0 w-full h-full rounded-lg bg-[#121212]" />
                 <div className="absolute z-2 bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.4)] left-0 w-full h-full rounded-lg" />
                 <div className="absolute w-full h-full [&>div]:w-auto [&>div]:h-auto z-1 flex items-center justify-center rounded-lg overflow-hidden [&_video]:-scale-x-100">
                     <VideoPreview
                         DisabledVideoPreview={() => (
-                            <div className="text-2xl text-white">{isVideoEnabled ? "Camera is starting..." : "Camera is off"}</div>
+                            <div className="text-2xl text-white">
+                                {videoPreviewText || (isVideoEnabled ? "Camera is starting..." : "Camera is off")}
+                            </div>
                         )}
+                    />
+                </div>
+                <div className="z-3 absolute bottom-4 left-1/2 -ml-17 flex items-center gap-6">
+                    <IconButton
+                        icon={isAudioEnabled ? <Mic className="w-5 h-5" /> : <MicOff className="w-5 h-5" />}
+                        title={isAudioEnabled ? "Turn off microphone" : "Turn on microphone"}
+                        onClick={toggleAudio}
+                        active={!isAudioEnabled}
+                        alert={!hasMicrophonePermission}
+                        variant="secondary"
+                    />
+                    <IconButton
+                        icon={isVideoEnabled ? <Video className="w-5 h-5" /> : <VideoOff className="w-5 h-5" />}
+                        title={isVideoEnabled ? "Turn off camera" : "Turn on camera"}
+                        onClick={toggleVideo}
+                        active={!isVideoEnabled}
+                        alert={!hasCameraPermission}
+                        variant="secondary"
                     />
                 </div>
                 {microphoneStatus === "enabled" && (
@@ -88,24 +121,21 @@ const MeetingPreview: React.FC = () => {
                         {user?.name}
                     </div>
                 )}
+                <div className="z-2 absolute top-2.5 right-1 [&>button]:w-12 [&>button]:h-12 [&>button]:border-none [&>button]:transition-none [&>button]:hover:bg-[rgba(255,255,255,.2)] [&>button]:hover:shadow-none">
+                    <IconButton title="More options" icon={<MoreVertical className="w-5 h-5" />} variant="secondary" />
+                </div>
+                <div className="z-3 absolute bottom-4 right-2.5">
+                    <IconButton icon={<Sparkles className="w-5 h-5" />} title="Apply visual effects" variant="secondary" />
+                </div>
             </div>
-            <div className="flex justify-between px-2 mt-4">
-                <button
-                    className={`${
-                        isVideoEnabled ? "bg-[#2C2C2C]" : "bg-red-500"
-                    } hover:bg-[#3C3C3C] transition-colors px-4 py-3 rounded-full flex items-center`}
-                    onClick={toggleVideo}
-                >
-                    {isVideoEnabled ? <Video className="w-5 h-5" /> : <VideoOff className="w-5 h-5" />}
-                </button>
-                <button
-                    className={`${
-                        isAudioEnabled ? "bg-[#2C2C2C]" : "bg-red-500"
-                    } hover:bg-[#3C3C3C] transition-colors px-4 py-3 rounded-full flex items-center`}
-                    onClick={toggleAudio}
-                >
-                    {isAudioEnabled ? <Mic className="w-5 h-5" /> : <MicOff className="w-5 h-5" />}
-                </button>
+            <div className="hidden lg:flex h-17 items-center gap-1 mt-4 ml-2">
+                {displaySelectors && (
+                    <>
+                        <AudioInputDeviceSelector disabled={!hasMicrophonePermission} />
+                        <AudioOutputDeviceSelector disabled={!hasMicrophonePermission} />
+                        <VideoInputDeviceSelector disabled={!hasCameraPermission} />
+                    </>
+                )}
             </div>
         </div>
     );
