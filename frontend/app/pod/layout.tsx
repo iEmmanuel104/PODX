b3260133ed41f6d8782600523135735b6e2fda67
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-12 07:17:10 +0100

    Refactor PodPage component to improve code readability and add support for joining a session with a meeting code

--- frontend/app/pod/layout.tsx
+++ frontend/app/pod/layout.tsx
@@ -1,15 +1,19 @@
 "use client";
 import { ReactNode } from "react";
-
+import { useSearchParams } from "next/navigation";
 import MeetProvider from "@/providers/meetProvider";
 
 type LayoutProps = {
     children: ReactNode;
     params: {
-        meetingId: string;
+        meetingId?: string;
     };
 };
 
 export default function Layout({ children, params }: LayoutProps) {
-    return <MeetProvider meetingId={params.meetingId}>{children}</MeetProvider>;
+    const searchParams = useSearchParams();
+    const code = searchParams.get("code");
+    const meetingId = params.meetingId || code || "";
+
+    return <MeetProvider meetingId={meetingId}>{children}</MeetProvider>;
 }
