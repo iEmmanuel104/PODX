886c48453d1f6d741732585570b9e4f0fcd232d6
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-12 18:46:37 +0100

    Refactor PodPage component to include session ID in setSessionInfo dispatch

--- frontend/app/pod/layout.tsx
+++ frontend/app/pod/layout.tsx
@@ -1,19 +1,24 @@
 "use client";
 import { ReactNode } from "react";
+import { useParams } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 import MeetProvider from "@/providers/meetProvider";
 
 type LayoutProps = {
     children: ReactNode;
     params: {
-        meetingId?: string;
+        id: string;
     };
 };
 
 export default function Layout({ children, params }: LayoutProps) {
     const searchParams = useSearchParams();
     const code = searchParams.get("code");
-    const meetingId = params.meetingId || code || "";
+
+    const { id } = useParams();
+    const meetingId = id as string || params.id || code || "";
+
+    console.log({ meetingId });
 
     return <MeetProvider meetingId={meetingId}>{children}</MeetProvider>;
 }
