f2e937d4d81ac45cef8abb7f282a834adc405e95
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-18 20:47:42 +0100

    Refactor connect wallet button label

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
                         ) : (
                             <>
                                 <Wallet className="w-5 h-5 mr-2" />
-                                Connect Wallet
+                                Connect
                             </>
                         )}
                     </button>
