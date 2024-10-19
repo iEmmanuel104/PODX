f2b39f4250ecd31df2d8ca395b25637b14df1fb7
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-19 23:18:47 +0100

    Refactor Logo component to use PodX logo instead of text

--- frontend/components/ui/logo.tsx
+++ frontend/components/ui/logo.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 const Logo = () => (
-    <h1 className="text-4xl font-bold mb-16 flex items-center">
-        <img src="/logo.png" alt="Podx" className="w-full h-full" />
+    <h1 className="text-3xl font-bold flex items-center space-x-2">
+        <span className="text-primary text-white">POD</span>
+        <div className="relative w-8 h-8">
+            <Image src="/logo.png" layout="fill" objectFit="contain" alt="Podx" />
+        </div>
     </h1>
 );
 
-export default Logo
+export default Logo;
