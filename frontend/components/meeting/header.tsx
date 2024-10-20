69e07dec11580bd0981198f4fd66c6b734702b27
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-20 02:41:18 +0100

    Refactor meeting header component to improve layout and optimize responsiveness

--- frontend/components/meeting/header.tsx
+++ frontend/components/meeting/header.tsx
@@ -31,25 +31,23 @@ const Header: React.FC<HeaderProps> = ({
     copyAddress,
 }) => {
     return (
-        <header className="flex justify-between items-center px-4 py-2 bg-[#1d1d1d] rounded-full w-[90%] mx-auto my-4">
-            <div className="flex items-center justify-between gap-4">
-                <Logo />
-                <p className="text-sm mr-2 hidden sm:inline w-full">{customData.title}</p>
-                <p className="bg-red-500 text-xs px-2 py-0.5 rounded-full">{live ? "Live" : "Offline"}</p>
+        <header className="flex justify-between items-center px-2 sm:px-4 py-2 bg-[#1d1d1d] rounded-full w-[95%] sm:w-[90%] mx-auto my-2 sm:my-4">
+            <div className="flex items-center justify-between gap-2 sm:gap-4">
+                <div className="w-6 h-6 sm:w-8 sm:h-8">
+                    <Logo />
+                </div>
+                <p className="text-xs sm:text-sm mr-1 sm:mr-2 hidden xs:inline max-w-[100px] sm:max-w-full truncate">{customData.title}</p>
+                <p className="bg-red-500 text-[10px] sm:text-xs px-1 sm:px-2 py-0.5 rounded-full">{live ? "Live" : "Offline"}</p>
             </div>
             <div className="flex items-center">
-                <button
-                    title="toggle sidebar"
-                    className="text-[#A3A3A3] hover:text-white transition-colors sm:hidden mr-2 sm:mr-4"
-                    onClick={toggleSidebar}
-                >
-                    <Menu className="w-6 h-6" />
+                <button title="toggle sidebar" className="text-[#A3A3A3] hover:text-white transition-colors sm:hidden mr-2" onClick={toggleSidebar}>
+                    <Menu className="w-5 h-5 sm:w-6 sm:h-6" />
                 </button>
                 <div className="relative">
                     <div className="flex items-center">
                         <button
                             title="toggle profile dropdown"
-                            className="flex items-center text-[#A3A3A3] hover:text-white transition-colors mr-2 sm:mr-4"
+                            className="flex items-center text-[#A3A3A3] hover:text-white transition-colors mr-1 sm:mr-2"
                             onClick={toggleProfileDropdown}
                         >
                             <Identity
@@ -57,42 +55,52 @@ const Header: React.FC<HeaderProps> = ({
                                 chain={base}
                                 schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
                             >
-                                <Avatar address={userAddress} chain={base} className="w-8 h-8 rounded-full mr-2" />
-                                <Name address={userAddress} chain={base} className="mr-2" />
+                                <Avatar address={userAddress} chain={base} className="w-6 h-6 sm:w-8 sm:h-8 rounded-full mr-1 sm:mr-2" />
+                                <Name
+                                    address={userAddress}
+                                    chain={base}
+                                    className="hidden xs:inline text-xs sm:text-sm mr-1 sm:mr-2 max-w-[60px] sm:max-w-full truncate"
+                                />
                             </Identity>
-                            <ChevronDown className="w-4 h-4" />
+                            <ChevronDown className="w-3 h-3 sm:w-4 sm:h-4" />
                         </button>
                         <div className="flex justify-between items-center">
-                            <LogOutIcon className="w-4 h-4 text-red-500" />
-                            <button onClick={handleLogout} className="text-red-500 px-3 py-1 rounded hover:bg-[#3d3d3d] transition-colors">
+                            <LogOutIcon className="w-3 h-3 sm:w-4 sm:h-4 text-red-500" />
+                            <button
+                                onClick={handleLogout}
+                                className="text-red-500 text-xs sm:text-sm px-2 sm:px-3 py-0.5 sm:py-1 rounded hover:bg-[#3d3d3d] transition-colors"
+                            >
                                 Logout
                             </button>
                         </div>
                     </div>
                     {isProfileDropdownOpen && (
-                        <div className="absolute right-0 mt-2 w-64 bg-[#2d2d2d] rounded-xl shadow-lg py-3 px-4 z-10">
+                        <div className="absolute right-0 mt-2 w-56 sm:w-64 bg-[#2d2d2d] rounded-xl shadow-lg py-2 sm:py-3 px-3 sm:px-4 z-10">
                             <div className="flex items-center">
                                 <Identity
                                     address={userAddress}
                                     chain={base}
                                     schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
                                 >
-                                    <Avatar address={userAddress} chain={base} className="w-10 h-10 rounded-full mr-3" />
-                                    <div className="space-y-2">
-                                        <div className="flex items-center justify-between gap-2">
-                                            <Name address={userAddress} chain={base} className="text-white font-semibold" />
-                                            <p className="text-white text-sm bg-violet-500 rounded-full px-2 py-0.5">
+                                    <Avatar address={userAddress} chain={base} className="w-8 h-8 sm:w-10 sm:h-10 rounded-full mr-2 sm:mr-3" />
+                                    <div className="space-y-1 sm:space-y-2">
+                                        <div className="flex items-center justify-between gap-1 sm:gap-2">
+                                            <Name address={userAddress} chain={base} className="text-white text-xs sm:text-sm font-semibold" />
+                                            <p className="text-white text-[10px] sm:text-xs bg-violet-500 rounded-full px-1 sm:px-2 py-0.5">
                                                 {displayBalance} {balanceSymbol}
                                             </p>
                                         </div>
-                                        <div className="flex items-center justify-between bg-[#1d1d1d] p-2 mb-2 rounded-full">
-                                            <Image src="/images/base.png" alt="Base" width={24} height={24} className="w-6 h-6" />{" "}
+                                        <div className="flex items-center justify-between bg-[#1d1d1d] p-1 sm:p-2 mb-1 sm:mb-2 rounded-full">
+                                            <Image src="/images/base.png" alt="Base" width={20} height={20} className="w-4 h-4 sm:w-6 sm:h-6" />
                                             <div className="flex items-center">
-                                                <div className="w-2 h-2 bg-green-500 rounded-full mr-2"></div>
-                                                <Address address={userAddress} className="text-[#A3A3A3] text-xs" />
+                                                <div className="w-1.5 h-1.5 sm:w-2 sm:h-2 bg-green-500 rounded-full mr-1 sm:mr-2"></div>
+                                                <Address
+                                                    address={userAddress}
+                                                    className="text-[#A3A3A3] text-[8px] sm:text-xs max-w-[80px] sm:max-w-[120px] truncate"
+                                                />
                                             </div>
                                             <button title="copy" onClick={copyAddress} className="text-[#A3A3A3] hover:text-white">
-                                                <Copy className="w-4 h-4" />
+                                                <Copy className="w-3 h-3 sm:w-4 sm:h-4" />
                                             </button>
                                         </div>
                                     </div>
