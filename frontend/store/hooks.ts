d0c6a3f03e0d09b9722dc2fc6871e18a360015e5
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-20 04:20:03 +0100

    Refactor hooks.ts to remove unused useSocketStatus function

--- frontend/store/hooks.ts
+++ frontend/store/hooks.ts
@@ -3,7 +3,3 @@ import type { TypedUseSelectorHook } from 'react-redux'
 import type { RootState, AppDispatch } from './index'
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-
-export const useSocketStatus = () => {
-    return useAppSelector((state) => state.socket.isConnected);
-};
