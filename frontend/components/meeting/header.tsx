0d8f0ff6ad2daddeb8adacc0b4b9f093320d499d
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-20 04:16:07 +0100

    Refactor meeting header component to improve layout and optimize responsiveness

--- frontend/components/meeting/header.tsx
+++ frontend/components/meeting/header.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Menu, ChevronDown, LogOutIcon, Copy } from "lucide-react";
+import React, { useState } from "react";
+import { Menu, ChevronDown, LogOutIcon, Copy, User } from "lucide-react";
 import { Avatar, Identity, Name, Address } from "@coinbase/onchainkit/identity";
 import { base } from "viem/chains";
 import Image from "next/image";
@@ -12,8 +12,6 @@ interface HeaderProps {
     displayBalance: string;
     balanceSymbol: string | undefined;
     toggleSidebar: () => void;
-    toggleProfileDropdown: () => void;
-    isProfileDropdownOpen: boolean;
     handleLogout: () => void;
     copyAddress: () => void;
 }
@@ -25,64 +23,51 @@ const Header: React.FC<HeaderProps> = ({
     displayBalance,
     balanceSymbol,
     toggleSidebar,
-    toggleProfileDropdown,
-    isProfileDropdownOpen,
     handleLogout,
     copyAddress,
 }) => {
+    const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+
+    const toggleProfileDropdown = () => {
+        setIsProfileDropdownOpen(!isProfileDropdownOpen);
+    };
+
     return (
-        <header className="flex justify-between items-center px-2 sm:px-4 py-2 bg-[#1d1d1d] rounded-full w-[95%] sm:w-[90%] mx-auto my-2 sm:my-4">
-            <div className="flex items-center justify-between gap-2 sm:gap-4">
-                <div className="w-6 h-6 sm:w-8 sm:h-8">
-                    <Logo />
-                </div>
-                <p className="text-xs sm:text-sm mr-1 sm:mr-2 hidden xs:inline max-w-[100px] sm:max-w-full truncate">{customData.title}</p>
+        <header className="flex justify-between items-center px-2 sm:px-4 py-2 bg-[#1d1d1d] rounded-full w-[98%] sm:w-[95%] mx-auto my-1 sm:my-2">
+            <div className="flex items-center justify-between gap-1 sm:gap-2">
+                <Logo />
+                <p
+                    className="text-xs sm:text-sm mr-1 sm:mr-2 w-24 sm:w-full sm:text-center truncate"
+                >
+                    {customData.title}
+                </p>
                 <p className="bg-red-500 text-[10px] sm:text-xs px-1 sm:px-2 py-0.5 rounded-full">{live ? "Live" : "Offline"}</p>
             </div>
             <div className="flex items-center">
-                <button title="toggle sidebar" className="text-[#A3A3A3] hover:text-white transition-colors sm:hidden mr-2" onClick={toggleSidebar}>
-                    <Menu className="w-5 h-5 sm:w-6 sm:h-6" />
-                </button>
                 <div className="relative">
-                    <div className="flex items-center">
+                    <button
+                        title="toggle profile dropdown"
+                        className="flex items-center text-[#A3A3A3] hover:text-white transition-colors"
+                        onClick={toggleProfileDropdown}
+                    >
+                        <User className="w-4 h-4 sm:w-5 sm:h-5 mr-1 sm:mr-2" />
                         <button
-                            title="toggle profile dropdown"
-                            className="flex items-center text-[#A3A3A3] hover:text-white transition-colors mr-1 sm:mr-2"
-                            onClick={toggleProfileDropdown}
+                            title="toggle sidebar"
+                            className="text-[#A3A3A3] hover:text-white transition-colors sm:hidden mr-1"
+                            onClick={toggleSidebar}
                         >
-                            <Identity
-                                address={userAddress}
-                                chain={base}
-                                schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
-                            >
-                                <Avatar address={userAddress} chain={base} className="w-6 h-6 sm:w-8 sm:h-8 rounded-full mr-1 sm:mr-2" />
-                                <Name
-                                    address={userAddress}
-                                    chain={base}
-                                    className="hidden xs:inline text-xs sm:text-sm mr-1 sm:mr-2 max-w-[60px] sm:max-w-full truncate"
-                                />
-                            </Identity>
-                            <ChevronDown className="w-3 h-3 sm:w-4 sm:h-4" />
-                        </button>
-                        <div className="flex justify-between items-center">
-                            <LogOutIcon className="w-3 h-3 sm:w-4 sm:h-4 text-red-500" />
-                            <button
-                                onClick={handleLogout}
-                                className="text-red-500 text-xs sm:text-sm px-2 sm:px-3 py-0.5 sm:py-1 rounded hover:bg-[#3d3d3d] transition-colors"
-                            >
-                                Logout
-                            </button>
-                        </div>
-                    </div>
+                            <Menu className="w-4 h-4 sm:w-5 sm:h-5" />
+                        </button>{" "}
+                    </button>
                     {isProfileDropdownOpen && (
-                        <div className="absolute right-0 mt-2 w-56 sm:w-64 bg-[#2d2d2d] rounded-xl shadow-lg py-2 sm:py-3 px-3 sm:px-4 z-10">
+                        <div className="absolute right-0 mt-2 w-64 bg-[#2d2d2d] rounded-xl shadow-lg py-2 sm:py-3 px-3 sm:px-4 z-10">
                             <div className="flex items-center">
                                 <Identity
                                     address={userAddress}
                                     chain={base}
                                     schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
                                 >
-                                    <Avatar address={userAddress} chain={base} className="w-8 h-8 sm:w-10 sm:h-10 rounded-full mr-2 sm:mr-3" />
+                                    <Avatar address={userAddress} chain={base} className="w-6 h-6 sm:w-8 sm:h-8 rounded-full mr-2" />
                                     <div className="space-y-1 sm:space-y-2">
                                         <div className="flex items-center justify-between gap-1 sm:gap-2">
                                             <Name address={userAddress} chain={base} className="text-white text-xs sm:text-sm font-semibold" />
@@ -91,13 +76,10 @@ const Header: React.FC<HeaderProps> = ({
                                             </p>
                                         </div>
                                         <div className="flex items-center justify-between bg-[#1d1d1d] p-1 sm:p-2 mb-1 sm:mb-2 rounded-full">
-                                            <Image src="/images/base.png" alt="Base" width={20} height={20} className="w-4 h-4 sm:w-6 sm:h-6" />
+                                            <Image src="/images/base.png" alt="Base" width={16} height={16} className="w-4 h-4 sm:w-5 sm:h-5" />
                                             <div className="flex items-center">
-                                                <div className="w-1.5 h-1.5 sm:w-2 sm:h-2 bg-green-500 rounded-full mr-1 sm:mr-2"></div>
-                                                <Address
-                                                    address={userAddress}
-                                                    className="text-[#A3A3A3] text-[8px] sm:text-xs max-w-[80px] sm:max-w-[120px] truncate"
-                                                />
+                                                <div className="w-1 h-1 sm:w-1.5 sm:h-1.5 bg-green-500 rounded-full mr-1 sm:mr-2"></div>
+                                                <Address address={userAddress} className="text-[#A3A3A3] text-[10px] sm:text-xs" />
                                             </div>
                                             <button title="copy" onClick={copyAddress} className="text-[#A3A3A3] hover:text-white">
                                                 <Copy className="w-3 h-3 sm:w-4 sm:h-4" />
@@ -105,6 +87,15 @@ const Header: React.FC<HeaderProps> = ({
                                         </div>
                                     </div>
                                 </Identity>
+                            </div>
+                            <div className="flex justify-center items-center mt-2">
+                                <LogOutIcon className="w-3 h-3 sm:w-4 sm:h-4 text-red-500 mr-1" />
+                                <button
+                                    onClick={handleLogout}
+                                    className="text-red-500 text-xs sm:text-sm px-2 sm:px-3 py-1 rounded hover:bg-[#3d3d3d] transition-colors"
+                                >
+                                    Logout
+                                </button>
                             </div>
                         </div>
                     )}
