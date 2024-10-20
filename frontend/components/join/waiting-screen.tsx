5be1a7978edc9454f3e6ac28d8d8ea4fadf1a0ed
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-10-20 21:05:20 +0100

    Refactor waiting screen background color

--- frontend/components/join/waiting-screen.tsx
+++ frontend/components/join/waiting-screen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const WaitingScreen: React.FC = () => (
-    <div className="w-full h-screen flex flex-col items-center justify-center text-center bg-gray-100">
+    <div className="w-full h-screen flex flex-col items-center justify-center text-center bg-[#121212]">
         <p className="text-lg mb-6">Please wait while we prepare your pod meeting experience.</p>
         <div className="animate-pulse text-blue-500">
             <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
