8b72cc5626ae4494c5ff5bf99517520a6b90b2d4
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-11-01 06:07:30 +0100

    feat: add scheduledSessions slice for managing scheduled session state

---
+++ frontend/store/slices/scheduledSessionSlice.ts
@@ -0,0 +1,30 @@
+// store/slices/scheduledSessionsSlice.ts
+import { StreamCallData } from '@/components/pod/scheduledPods';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface scheduledSessionsState {
+    sessions: StreamCallData[];
+}
+
+const initialState: scheduledSessionsState = {
+    sessions: []
+};
+
+const scheduledSessionsSlice = createSlice({
+    name: 'scheduledSessions',
+    initialState,
+    reducers: {
+        setScheduledSessions: (state, action: PayloadAction<StreamCallData[]>) => {
+            state.sessions = action.payload;
+        },
+        addScheduledSession: (state, action: PayloadAction<StreamCallData>) => {
+            state.sessions.push(action.payload);
+        },
+        clearScheduledSessions: (state) => {
+            state.sessions = [];
+        }
+    }
+});
+
+export const { setScheduledSessions, addScheduledSession, clearScheduledSessions } = scheduledSessionsSlice.actions;
+export default scheduledSessionsSlice.reducer;
