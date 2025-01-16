9f6603bd28a92f62b79565f3f316fc019dc0a3c8
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2025-01-16 15:24:56 +0100

    Revert "add poap contract"
    This reverts commit 976436f5b7cea1c3492741a1c87ab32e5c8ac8e1.

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -13,67 +13,64 @@ import X from "@/assets/icons/socials/X";
 import styles from "./styles.module.scss";
 
 const balige = localFont({
-  src: "../fonts/Balige - Personal Use.otf",
-  variable: "--font-balige",
+    src: "../fonts/Balige - Personal Use.otf",
+    variable: "--font-balige",
 });
 
 export default function LandingPage() {
-  const dispatch = useAppDispatch();
-  const { login, logout, ready } = usePrivy();
+    const dispatch = useAppDispatch();
+    const { login, logout, ready } = usePrivy();
 
-  const handleConnect = useCallback(async () => {
-    try {
-      await logout(); // Log out of existing privy session
-      dispatch(logOut()); // clear user data from store
-      await login();
-    } catch (error) {
-      console.error("Error connecting wallet:", error);
-    }
-  }, [logout, dispatch, login]);
+    const handleConnect = useCallback(async () => {
+        try {
+            await logout(); // Log out of existing privy session
+            dispatch(logOut()); // clear user data from store
+            await login();
+        } catch (error) {
+            console.error("Error connecting wallet:", error);
+        }
+    }, [logout, dispatch, login]);
 
-  if (!ready) return null;
+    if (!ready) return null;
 
-  return (
-    <>
-      <main className={`${styles.main} ${balige.variable}`}>
-        <div className="container mx-auto">
-          <div className={styles.contentWrapper}>
-            <div className={styles.contents}>
-              <div className={styles.logo}>
-                <Logo />
-              </div>
-              <div className="flex flex-col gap-[68px]">
-                <div className="flex flex-col justify-center items-center gap-[24px]">
-                  <div className="flex justify-center items-center rounded-full bg-gradient-to-r from-[#552FC9]  to-[#D7B35D] p-[1px]">
-                    <span className="rounded-full bg-[#212121] text-white text-xs uppercase tracking-wider py-[4px] px-[16px] ">
-                      A creator's workspace
-                    </span>
-                  </div>
-                  <h1 className="text-4xl lg:text-h1 text-center font-balige lg:leading-[45px] text-light-gray">
-                    Host meetings, record sessions, earn proof of attendance,
-                    and{" "}
-                    <span className="inline-block">
-                      <span className="bg-gradient-to-r from-[#D7B35D] to-[#552FC9] text-transparent bg-clip-text">
-                        tip
-                      </span>
-                    </span>{" "}
-                    seamlessly
-                  </h1>
+    return (
+        <>
+            <main className={`${styles.main} ${balige.variable}`}>
+                <div className="container mx-auto">
+                    <div className={styles.contentWrapper}>
+                        <div className={styles.contents}>
+                            <div className={styles.logo}>
+                                <Logo />
+                            </div>
+                            <div className="flex flex-col gap-[68px]">
+                                <div className="flex flex-col justify-center items-center gap-[24px]">
+                                    <div className="flex justify-center items-center rounded-full bg-gradient-to-r from-[#552FC9]  to-[#D7B35D] p-[1px]">
+                                        <span className="rounded-full bg-[#212121] text-white text-xs uppercase tracking-wider py-[4px] px-[16px] ">
+                                            A creator's workspace
+                                        </span>
+                                    </div>
+                                    <h1 className="text-4xl lg:text-h1 text-center font-balige lg:leading-[45px] text-light-gray">
+                                        Host meetings, record sessions, earn proof of attendance, and{" "}
+                                        <span className="inline-block">
+                                            <span className="bg-gradient-to-r from-[#D7B35D] to-[#552FC9] text-transparent bg-clip-text">tip</span>
+                                        </span>{" "}
+                                        seamlessly
+                                    </h1>
+                                </div>
+
+                                <div className="flex justify-center">
+                                    <button
+                                        className="py-2 px-8 rounded-[10px] bg-[#6032F6] hover:bg-[#4C28C4] transition-colors text-white font-medium text-lg"
+                                        onClick={handleConnect}
+                                    >
+                                        Get started
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="flex justify-center">
-                  <button
-                    className="py-2 px-8 rounded-[10px] bg-[#6032F6] hover:bg-[#4C28C4] transition-colors text-white font-medium text-lg"
-                    onClick={handleConnect}
-                  >
-                    Get started
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-    </>
-  );
+            </main>
+        </>
+    );
 }
