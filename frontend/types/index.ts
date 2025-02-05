51e0df680eb4fea86df87a672397e974952ee467
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2025-02-05 21:32:28 +0100

    feat: add TypeScript interfaces for user state and wallet operations

---
+++ frontend/types/index.ts
@@ -0,0 +1,24 @@
+import { UserInfo } from '@/store/api/userApi';
+import React from 'react';
+
+
+export interface UserState {
+    isOpen: boolean;
+    isSettingsOpen: boolean;
+    isWarningOpen: boolean;
+    isWithdrawOpen: boolean;
+    amount: string;
+    address: string;
+}
+
+export interface UserDetailsProps {
+    user: UserInfo;
+}export interface WalletOperationsProps {
+    state: UserState;
+    setState: React.Dispatch<React.SetStateAction<UserState>>;
+    user: UserInfo;
+    onWithdrawClick: () => void;
+    onWarningConfirm: () => void;
+    isBalanceHidden?: boolean;
+}
+
