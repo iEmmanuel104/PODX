dee58bf141c6f1ec9b3847d4b23433c86eccb7d3
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2025-02-05 18:04:02 +0100

    feat: add streak routes for leaderboard and recalculation functionalities

---
+++ server/src/routes/streak.routes.ts
@@ -0,0 +1,13 @@
+import express, { Router } from 'express';
+import { StreakController } from '../controllers/streak.controller';
+import { AuthenticatedController, basicAuth } from '../middlewares/authMiddleware';
+
+const router: Router = express.Router();
+
+router
+    .get('/leaderboard', StreakController.getLeaderboard)
+    .post('/recalculate', basicAuth(), AuthenticatedController(StreakController.recalculateUserStreak))
+    .post('/recalculate-all', StreakController.recalculateAllStreaks)
+    .post('/sync-streamio', StreakController.syncStreaksWithStreamIO);
+
+export default router;
