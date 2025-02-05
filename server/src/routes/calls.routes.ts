b3a423a22ed24b8d12f939d5006e7b97db720a3e
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2025-02-05 19:10:21 +0100

    feat: update call routes to improve clarity and organization of endpoints

--- server/src/routes/calls.routes.ts
+++ server/src/routes/calls.routes.ts
@@ -8,21 +8,20 @@ const router: Router = express.Router();
 router
     .post('/schedule', basicAuth(), AuthenticatedController(CallsController.scheduleCall))
     .get('/scheduled', basicAuth(), AuthenticatedController(CallsController.getUserScheduledCalls))
-    .get('/:sessionId', basicAuth(), AuthenticatedController(CallsController.getCall));
+    .get('/info/:sessionId', basicAuth(), AuthenticatedController(CallsController.getCall))
 
 // Stream call management routes
-router
+
     .post('/create', basicAuth(), AuthenticatedController(CallsController.createCall))
     .post('/get-or-create', basicAuth(), AuthenticatedController(CallsController.getOrCreateCall))
     .patch('/settings', basicAuth(), AuthenticatedController(CallsController.updateCallSettings))
     .patch('/members', basicAuth(), AuthenticatedController(CallsController.updateCallMembers))
-    .post('/end', basicAuth(), AuthenticatedController(CallsController.endCall));
+    .post('/end', basicAuth(), AuthenticatedController(CallsController.endCall))
 
 // Call information routes
-router
+
     .get('/stats', CallsController.getCallStats)
     .get('/detailed-stats', CallsController.getDetailedCallStats)
-    .get('/:callId', CallsController.getCallDetails)
     .post('/members', CallsController.queryCallMembers);
 
 export default router;
